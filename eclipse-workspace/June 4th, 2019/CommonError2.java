74aebbfa240501e298738a698ab62b3977263a7e
Vincent Nguyen <vincentn98@gmail.com>
2019-07-20 19:27:52 -0500

    7/20/19

--- eclipse-workspace/June 4th, 2019/CommonError2.java
+++ eclipse-workspace/June 4th, 2019/CommonError2.java
@@ -2,8 +2,9 @@
 public class CommonError2 {
 
 	public static void main(String[] args) {
-		double interestRate = 0.05;
-		double interest = interestrate*45;
+		//double interestRate = 0.05;
+		//double interest = interestRate*45;
+		//double interest = interestrate*45;
 		// TODO Auto-generated method stub
 
 	}
